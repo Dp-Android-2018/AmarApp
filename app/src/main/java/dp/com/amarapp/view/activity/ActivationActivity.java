1c1912c035915190705d0b5db49766db76cf1ffc
bahaa_gabal <bahaa.gabal@yahoo.com>
2018-08-30 17:06:11 +0200

    123 message

--- app/src/main/java/dp/com/amarapp/view/activity/ActivationActivity.java
+++ app/src/main/java/dp/com/amarapp/view/activity/ActivationActivity.java
@@ -19,6 +19,7 @@ public class ActivationActivity extends AppCompatActivity implements BaseInterfa
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.println("Hello Amar");
         initBinding();
     }
 
